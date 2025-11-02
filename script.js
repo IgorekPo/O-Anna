299cbb7de45601680fb3ab6f47edfcfe88ccc881
Igor_Polyakov <itworldigpo@gmail.com>
2025-11-02 14:06:03 +0200

    const

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 const burgerMenu = document.querySelector ('.header__burger');
 const headerMenu = document.querySelector ('.header__menu')
-
+const inputTel = document.querySelector ('.consultation__input')
 burgerMenu.addEventListener ('click' , function(){
    burgerMenu.classList.toggle ('active');
-   headerMenu.classList.toggle ('active')
+   headerMenu.classList.toggle ('active');
+
+})
+
+inputTel.addEventListener ('click', function(){
+   inputTel.classList.add ('active');
 })

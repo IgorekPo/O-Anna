2a8a30bbcd43bd183c5d4072454d052f9140f356
Igor_Polyakov <itworldigpo@gmail.com>
2025-11-23 21:01:54 +0200

    asd

--- price.js
+++ price.js
@@ -3,5 +3,12 @@ new Swiper('.swiper',{
   pagination: {
     el: '.swiper-pagination',
     clickable: true,
+    renderBullet: function (index, className) {
+      // Создайте массив с вашими названиями
+      const buttonNames = ['Особистий', 'Корпоративний', 'Саморозвиток'];
+      // Верните HTML-код с названием из массива
+      return '<span class="' + className + '">' + buttonNames[index] + '</span>';
+    },
+
   },  
 });
